74728556f2ae0e1d5c22c0c2ea290d7bea8b297d
dwetoskey <dwetoskey224@gmail.com>
2022-03-18 14:46:20 -0400

    getting error when trying to node, added test for employee

--- index.js
+++ index.js
@@ -1,11 +1,154 @@
-const manager = require("./lib/manager.js");
+const fs = require('fs');
+const path = require('path');
 const employee = require('./lib/employee.js');
+const manager = require("./lib/manager.js");
 const intern = require('./lib/intern');
 const engineer = require('./lib/engineer.js');
 const inquirer = require("inquirer");
-const fs = require('fs');
-const path = require('path');
-const dirOut = path.resolve(__dirname, "output");
+const dirOut = path.resolve(__dirname, "dist");
 const pathOut = path.join(dirOut, "index.html");
 const generate = require('./src/page-template.js');
+
+team = [];
+
+function run() {
+
+    function teambuild() {
+        inquirer.prompt([{
+            type: "list",
+            message: "What role would you like to add?",
+            name: "addemployee",
+            choices: ["manager", "engineer", "intern", "I'm finished."]
+        }])
+        .then(function(input) {
+            switch(input.addemployee) {
+                case "manager":
+                    addmanager();
+                    break;
+                
+                case "engineer":
+                    addengineer();
+                    break;
+                
+                case "intern":
+                    addintern();
+                    break;
+
+                default: buildhtml();
+
+            }
+        })
+    }
+
+    function addmanager() {
+        inquirer.prompt ([
+        
+            {
+                type: "input",
+                name: "managername",
+                message: "Enter manager's name."
+            },
+
+            {
+                type: "input",
+                name: "managerid",
+                message: "Enter the manager's employee Id."
+            },
+
+            {
+                type: "input",
+                name: "manageremail",
+                message: "Enter the manager's email."
+            },
+
+            {
+                type: "input",
+                name: "manageroffice",
+                message: "Enter the manager's office number."
+            }
+        ])
+        .then(answers => {
+            const managerinfo = new manager(answers.managername, answers.managerid, answers.manageremail, answers.manageroffice);
+            team.push(managerinfo);
+            teambuild();
+        });
+    }
+
+    function addengineeer() {
+        inquirer.prompt ([
+        
+            {
+                type: "input",
+                name: "engineername",
+                message: "Enter engineer's name."
+            },
+
+            {
+                type: "input",
+                name: "engineerid",
+                message: "Enter the engineer's employee Id."
+            },
+
+            {
+                type: "input",
+                name: "engineeremail",
+                message: "Enter the engineer's email."
+            },
+
+            {
+                type: "input",
+                name: "engineergit",
+                message: "Enter the engineer's github user name."
+            }
+        ])
+        .then(answers => {
+            const engineerinfo = new engineer(answers.engineername, answers.engineerid, answers.engineeremail, answers.engineergit);
+            team.push(engineerinfo);
+            teambuild();
+        });
+    }
+
+    function addintern() {
+        inquirer.prompt ([
+        
+            {
+                type: "input",
+                name: "interernname",
+                message: "Enter intern's name."
+            },
+
+            {
+                type: "input",
+                name: "internid",
+                message: "Enter the intern's employee Id."
+            },
+
+            {
+                type: "input",
+                name: "internemail",
+                message: "Enter the intern's email."
+            },
+
+            {
+                type: "input",
+                name: "internschool",
+                message: "Enter the intern's alma mater."
+            }
+        ])
+        .then(answers => {
+            const interninfo = new intern(answers.internname, answers.internid, answers.internemail, answers.internschool);
+            team.push(interninfo);
+            teambuild();
+        });
+    }
+
+    function buildhtml() {
+        
+        fs.writeFileSync(pathOut, generate(team), "UTF-8")
+    }
+
+    teambuild();
+}
+
+run();
 
--- src/page-template.js
+++ src/page-template.js
@@ -5,7 +5,7 @@ const generateTeamProfile = team => {
         return
     
    
-        <div class="tile is-parent ">
+        <div class="tile is-parent is-vertical ">
             <div class="tile is-child has-text-centered has-background-success box">
                     
                     <div>
@@ -22,13 +22,14 @@ const generateTeamProfile = team => {
                 </div>
 
             </div>
+            ;
   
-    }
+    };
 
     const generateengineer = engineer => {
 
 
-        <div class="tile is-parent ">
+        <div class="tile is-parent is-vertical">
             <div class="tile is-child has-text-centered has-background-success box">
                     
                     <div>
@@ -45,7 +46,88 @@ const generateTeamProfile = team => {
                 </div>
 
             </div>
+            ;
 
-    }
+    };
 
+    const generateintern = intern => {
+
+        <div class="tile is-parent is-vertical">
+            <div class="tile is-child has-text-centered has-background-success box">
+                    
+                    <div>
+                        <h1 class ="title is-2">${intern.getname()}</h1>
+                        <h1 class ="title is-3">${intern.getrole()}</h1>
+                        <h2 class ="title is-4">${intern.getid()}</h2> 
+                    </div>
+
+                    <div>
+                        <li class="is-size-4">Email: <a href="mailto:${intern.getemail()}"></a> </li>
+                        <li class="is-size-4">School: ${intern.getschool()}</li>
+                    </div>
+                    
+                </div>
+
+            </div>
+            ;
+
+    };
+
+    const page = [];
+
+    page.push(team
+        .filter(employee => employee.getrole() === "manager")
+        .map(manager => generatemanager(manager))
+        );
+
+        page.push(team
+            .filter(employee => employee.getrole() === "engineer")
+            .map(engineer => generateengineer(engineer))
+            .join("")
+        );
+
+        page.push(team
+            .filter(employee => employee.getrole() === "intern")
+            .map(intern => generateintern(intern))
+            .join("")
+        );
+        
+        return page.join("");
+}
+
+module.exports = team => {
+
+
+    return
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.3/css/bulma.min.css">
+
+</head>
+
+<body class="has-background-warning">
+    <div class="container has-background-info">
+        <h1 class="has-text-primary-light title is-1 has-text-centered">The Team</h1>
+    </div>
+    <br>
+    <div class ="container is-fluid">
+        <div class="tile is-ancestor">
+
+        ${generateTeamProfile(team)}
+
+
+        </div>
+    </div>
+</body>
+</html>
+
+    ;
 }
