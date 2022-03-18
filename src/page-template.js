58af05e7e0fd1d9a49e692d40bc8c469aaee3c3b
dwetoskey <dwetoskey224@gmail.com>
2022-03-18 11:55:04 -0400

    added engineer and manager roles to page-template and worked on formatting with bulma

--- src/page-template.js
+++ src/page-template.js
@@ -1,8 +1,51 @@
 const generateTeamProfile = team => {
-    if (!team) {
-        return '';
+
+    const generatemanager = manager => {
+
+        return
+    
+   
+        <div class="tile is-parent ">
+            <div class="tile is-child has-text-centered has-background-success box">
+                    
+                    <div>
+                        <h1 class ="title is-2">${manager.getname()}</h1>
+                        <h1 class ="title is-3">${manager.getrole()}</h1>
+                        <h2 class ="title is-4">${manager.getid()}</h2> 
+                    </div>
+
+                    <div>
+                        <li class="is-size-4">Email: <a href="mailto:${manager.getemail()}"></a> </li>
+                        <li class="is-size-4">Office: ${manager.getoffice()}</li>
+                    </div>
+                    
+                </div>
+
+            </div>
+  
     }
 
-    return
-    <section class ="my-3"
+    const generateengineer = engineer => {
+
+
+        <div class="tile is-parent ">
+            <div class="tile is-child has-text-centered has-background-success box">
+                    
+                    <div>
+                        <h1 class ="title is-2">${engineer.getname()}</h1>
+                        <h1 class ="title is-3">${engineer.getrole()}</h1>
+                        <h2 class ="title is-4">${engineer.getid()}</h2> 
+                    </div>
+
+                    <div>
+                        <li class="is-size-4">Email: <a href="mailto:${engineer.getemail()}"></a> </li>
+                        <li class="is-size-4">Github: <a href = "${enginner.getgit()}" target="_blank"></a></li>
+                    </div>
+                    
+                </div>
+
+            </div>
+
+    }
+
 }
